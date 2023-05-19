f1a27bddb36490bfa7fa0bc46c772867ecb7dbe4
AndreyGolovin85 <47321231+AndreyGolovin85@users.noreply.github.com>
2023-05-19 23:49:05 +0300

    Добавлены миграции

--- MySite/settings.py
+++ MySite/settings.py
@@ -125,5 +125,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+AUTH_USER_MODEL = 'service.User'
+
 # Для поля "birth_date" устанавливаем минимальный возраст
 USER_MIN_AGE = 9
